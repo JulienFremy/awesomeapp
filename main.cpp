c1da88fcb503419a16027d0db486a3c53eca1ec3
Julien Fremy <julien.fremy@myscript.com>
2016-02-09 17:40:51 +0100

    Make a tiny example of qibuild library system

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
+#include <libfoo/libfoo.hpp>
+#include <libbar/libbar.hpp>
+
+#include <iostream>
+
 int main(int argc, char* argv[])
 {
+  std::cout << hello() << ", " << world() << "!" << std::endl;
   return 0;
 }
